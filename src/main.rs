d7cc50dc646a4b2e9ff0b8ed22006134b8d7adce
Oscar David Arbeláez Echeverri <odarbelaeze@gmail.com>
2017-04-05 22:19:17 -0500

    Implement acceleration.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+extern crate nalgebra;
+use nalgebra::{Vector3, norm, dot};
+
+fn acceleration(r: &Vector3<f64>) -> Vector3<f64> {
+    let weigth = Vector3::new(0.0, 0.0, -9.8);
+    let tension = - dot(&weigth, r) / norm(r).powi(2) * r;
+    tension + weigth
+}
+
 fn main() {
-    println!("Hello, world!");
+    let r = Vector3::new(0.1, 0.0, -1.0);
+    let a = acceleration(&r);
+    println!("Acceleration {}", a);
 }
